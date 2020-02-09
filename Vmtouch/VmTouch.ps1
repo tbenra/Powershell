98f2c907d365c37b4a4f72e16530ef1563ad1e11
tbenra <52466686+tbenra@users.noreply.github.com>
2020-02-09 21:09:23 +0100

    Init

---
+++ Vmtouch/VmTouch.ps1
@@ -0,0 +1,13 @@
+<# Computer\HKEY_CLASSES_ROOT\Directory\Background\shell\Vm_touch erstelle
+    (Standard) Namen vergeben
+    Neuen (Unter)Schlüssel anlegen: command 
+    (Standard): Powershell "C:\Pfad\zum\Skript\VmTouch.ps1"
+#>
+$PathToShare = "/Nas/Data"
+
+$path = (Get-Location).Path.Replace("\","/").Replace((Get-Location).Drive.Name,$PathToShare).Replace(":","")
+Write-Host $path
+ssh tobias@192.168.10.1 vmtouch -vt $path
+Write-Host "Beliebige Taste zum Beenden druecken"
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+
